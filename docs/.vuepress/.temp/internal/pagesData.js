23b06bb6f3db97853647e875e147c1e49eeca9bf
MoeMagicMango <i@himoyo.cn>
2022-08-06 11:08:22 +0800

    Delete docs directory

--- docs/.vuepress/.temp/internal/pagesData.js
+++
@@ -1,8 +0,0 @@
-export const pagesData = {
-  // path: /
-  "v-8daa1a0e": () => import(/* webpackChunkName: "v-8daa1a0e" */"C:/Users/MoeMagicMango/Desktop/LoadInstruction/vuepress_lucyManualBook/docs/.vuepress/.temp/pages/index.html.js").then(({ data }) => data),
-  // path: /show/
-  "v-15488579": () => import(/* webpackChunkName: "v-15488579" */"C:/Users/MoeMagicMango/Desktop/LoadInstruction/vuepress_lucyManualBook/docs/.vuepress/.temp/pages/show/index.html.js").then(({ data }) => data),
-  // path: /404.html
-  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"C:/Users/MoeMagicMango/Desktop/LoadInstruction/vuepress_lucyManualBook/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
-}
